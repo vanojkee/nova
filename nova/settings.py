f136eaa943d09e6da57af9159d76612451829839
Ivan Zubenko <zubenkoivan24@gmail.com>
2022-07-14 12:42:44 +0300

    Change ip version2.

--- nova/settings.py
+++ nova/settings.py
@@ -133,8 +133,8 @@ WEBHOOK_PATH = ''
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 # webserver settings
-WEBAPP_HOST = 'localhost'  # or ip
-WEBAPP_PORT = 3001
+WEBAPP_HOST = '0.0.0.0'  # or ip
+WEBAPP_PORT = os.getenv('PORT', default=8000)
 
 # Heroku settings
 django_heroku.settings(locals())
